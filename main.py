362995d6acf8475be3d9874116633654b659e1a8
Дарья <dashkasuvorova307@gmail.com>
2020-11-26 16:57:54 +0300

    last version

---
+++ main.py
@@ -0,0 +1,42 @@
+import sys
+
+from PyQt5 import uic  # Импортируем uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)  # Загружаем дизайн
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            # Создаем объект QPainter для рисования
+            qp = QPainter()
+            # Начинаем процесс рисования
+            qp.begin(self)
+            self.draw_flag(qp)
+            # Завершаем рисование
+            qp.end()
+
+    def draw_flag(self, qp):
+        # Задаем кисть
+        qp.setBrush(QColor(255, 255, 173))
+        # Рисуем прямоугольник заданной кистью
+        n = randint(0, 400)
+        qp.drawEllipse(randint(0, 700), randint(0, 500), n, n)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
